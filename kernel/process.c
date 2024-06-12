5b282c2344eaa72fc77e9aca8be4d5bd4b43af0b
aiichi <parimals7411@gmail.com>
2024-06-12 08:35:32 +0530

    Temp fix for whyred

--- kernel/process.c
+++ kernel/process.c
@@ -8,9 +8,9 @@
 #define ARC_PATH_MAX 256
 
 extern struct mm_struct *get_task_mm(struct task_struct *task);
+extern void mmput(struct mm_struct *);
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 61))
-extern void mmput(struct mm_struct *);
 #endif
 
 uintptr_t get_module_base(pid_t pid, char *name)
